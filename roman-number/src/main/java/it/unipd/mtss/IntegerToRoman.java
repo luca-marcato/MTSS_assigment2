2f4fb864e1917ee209248a141b1c15d55d9f6d6d
Marcato Luca <luca.marcato.2@studenti.unipd.it>
2023-05-12 09:52:41 +0200

    fase develop TDD: primiSeiNumeri

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -8,6 +8,14 @@ package it.unipd.mtss;
 public class IntegerToRoman {
   public static String convert(int number){
     String result = "";
+    while (number >= 5) {
+        result += "V";
+        number -= 5;
+    }
+    while (number >= 4) {
+        result += "IV";
+        number -= 4;
+    }
     while (number >= 1) {
         result += "I";
         number -= 1;
