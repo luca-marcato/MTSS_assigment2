77cd663dc6c66f6b8de49da7a319aa7dee2d1630
Mircea <plmn170999@gmail.com>
2023-05-12 10:45:48 +0200

    added mockito

---
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// Mircea Plamadeala 2009996
+// Luca Marcato 2014720
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        // TODO
+        return null;
+    }
+}
