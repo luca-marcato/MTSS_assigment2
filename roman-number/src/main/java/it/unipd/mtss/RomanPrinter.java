1622302058149d1356cf9d6c83bdde4b7fe9def6
Mircea <plmn170999@gmail.com>
2023-05-12 11:05:43 +0200

    fase refactoring TDD: carattere_V

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -12,21 +12,23 @@ public class RomanPrinter {
 
     private static String printAsciiArt(String romanNumber) {
         String[] value = { "", "", "", "", "", "" };
-        if (romanNumber.charAt(0) == 'I') {
-            value[0] += " _____  ";
-            value[1] += "|_   _| ";
-            value[2] += "  | |   ";
-            value[3] += "  | |   ";
-            value[4] += " _| |_  ";
-            value[5] += "|_____| ";
-        }
-        if (romanNumber.charAt(0) == 'V') {
-            value[0] += "__      __ ";
-            value[1] += "\\ \\    / / ";
-            value[2] += " \\ \\  / /  ";
-            value[3] += "  \\ \\/ /   ";
-            value[4] += "   \\  /    ";
-            value[5] += "    \\/     ";
+        switch (romanNumber.charAt(0)) {
+            case 'I':
+                value[0] += " _____  ";
+                value[1] += "|_   _| ";
+                value[2] += "  | |   ";
+                value[3] += "  | |   ";
+                value[4] += " _| |_  ";
+                value[5] += "|_____| ";
+                break;
+            case 'V':
+                value[0] += "__      __ ";
+                value[1] += "\\ \\    / / ";
+                value[2] += " \\ \\  / /  ";
+                value[3] += "  \\ \\/ /   ";
+                value[4] += "   \\  /    ";
+                value[5] += "    \\/     ";
+                break;
         }
         return value[0] + "\n" + value[1] + "\n" + value[2] + "\n" + value[3] + "\n" + value[4] + "\n" + value[5];
     }
